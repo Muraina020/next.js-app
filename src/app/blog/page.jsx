cb873f9e1a7291a5c8fbc3d1bb5f114292b975e0
Muraina020 <glonal-murainaboluwatife@gmail.com>
2024-06-11 08:10:34 +0100

    first changes

--- src/app/blog/page.jsx
+++ src/app/blog/page.jsx
@@ -3,15 +3,27 @@ import styles from "./page.module.css"
 import Link from 'next/link'
 import Image from 'next/image'
 
+// async function getData() {
+//   const res = await fetch('http://localhost:3000/api/posts', { cache: 'no-store' })
+ 
+//   if (!res.ok) {
+//     throw new Error('Failed to fetch data')
+//   }
+ 
+//   return res.json()
+// }
+
 async function getData() {
-  const res = await fetch('http://localhost:3000/api/posts', { cache: 'no-store' })
- 
+  const API_URL = process.env.NEXT_PUBLIC_API_URL;
+  const res = await fetch(`${API_URL}/api/posts`, { cache: 'no-store' });
+
   if (!res.ok) {
-    throw new Error('Failed to fetch data')
+    throw new Error('Failed to fetch data');
   }
- 
-  return res.json()
+
+  return res.json();
 }
+
 
 const Blog = async () => {
 

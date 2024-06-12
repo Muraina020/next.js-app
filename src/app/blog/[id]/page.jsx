26f03dfcece65d650415de98eb5ebff340958ea1
Muraina020 <glonal-murainaboluwatife@gmail.com>
2024-06-12 14:35:05 +0100

    mnew changes

--- src/app/blog/[id]/page.jsx
+++ src/app/blog/[id]/page.jsx
@@ -4,7 +4,9 @@ import Image from 'next/image'
 import { notFound } from 'next/navigation'
 
 async function getData(id) {
-  const res = await fetch(`http://localhost:3000/api/posts/${id}`, { cache: 'no-store' })
+  // const res = await fetch(`http://localhost:3000/api/posts/${id}`, { cache: 'no-store' })
+
+  const res = await fetch(`${process.env.API_BASE_URL}/api/posts/${id}`, { cache: 'no-store' })
  
   if (!res.ok) {
     return notFound()
